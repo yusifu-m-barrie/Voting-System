02d9bec4f72314bb3205810c8320caee1f2c6f56
yusifu-m-barrie <usifubarriem@gmail.com>
2020-12-11 17:38:23 -0800

    adding content

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+      <div class="pull-right hidden-xs">
+        <b>Online Voting System</b>
+      </div>
+      <strong> &copy; 2020 Brought To You By<a href="http://usifumbarrie.epizy.com"> Yusifu M. Barrie & <a href="#">Abubakarr S. Kamara</a></a></strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Voting System</b>
+      </div>
+      <strong> &copy; 2020 Brought To You By<a href="http://usifumbarrie.epizy.com"> Yusifu M. Barrie and <a href="#">Abubakarr S. Kamara</a></a></strong>
+    </div>
+    <!-- /.container -->
+</footer>
